162bfb0b95d65a25763b1310bf8bc963f4a1bb6a
stephweb <stephweb@live.fr>
2017-04-18 17:14:16 +0200

    Add Observer pattern

--- src/observer/index.php
+++ src/observer/index.php
@@ -3,3 +3,28 @@
 /**
  * Observer
  */
+
+require_once 'File/Observers/ObserverInterface.php';
+require_once 'File/Observers/AudioObserver.php';
+require_once 'File/Observers/ImageObserver.php';
+require_once 'File/Observers/VideoObserver.php';
+require_once 'File/File.php';
+
+use File\File;
+use File\Observers\AudioObserver;
+use File\Observers\ImageObserver;
+use File\Observers\VideoObserver;
+
+
+$audioObserver = new AudioObserver();
+
+$imageObserver = new ImageObserver();
+
+$videoObserver = new VideoObserver();
+
+$file = new File();
+$file->attach($audioObserver);
+$file->attach($imageObserver);
+$file->attach($videoObserver);
+$file->dettach($audioObserver);
+$file->setName('Fichier 1');
